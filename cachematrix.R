7613fa184224ffed874299b13215e7e2543f891e
Radu Craioveanu <raducraioveanu@Radu-Craioveanus-MacBook-Pro.local>
2015-04-22 08:11:04 -0400

    built and tested the code that creates a cached inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions (makeCacheMatrix and cachesolve) 
+## will work together to allow for fast retrieval of an
+## inverse matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix will build a matrix that is capable of creating
+## it's own inverse and cache it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ## initialize in scope vars 
+  inv <- NULL
+  
+  ## build your setters and getters for the matrix and it's invers
+  set <- function(y) {
+    ## init your out of scope (global) vars
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  
+  ## instantiate setinverse and getinvers
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will return the inverse of a matrix
+## if the matrix is in cache already, it will return it
+## otherwise it will build it, cache it, and return it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  ## get the inverse from cache
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  
+  ## if no cache, create the matrix, inverse it, and copy to cache
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv  
 }
